1944c8ccf91757f54ee799f69677fcf36b3d8f3d
Fadami <rossyfranca@gmail.com>
2025-07-29 11:26:02 -0300

    first commit

---
+++ src/main/java/com/franca/spring_file_processor/SpringFileProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.franca.spring_file_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFileProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFileProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/franca/spring_file_processor/SpringFileProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franca.spring_file_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFileProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
